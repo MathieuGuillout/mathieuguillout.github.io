961b50f1e29820552218e1472a9721534aae5d89
MathieuGuillout <mathieu@creativelicence.com.au>
2014-06-26 01:51:15 +0200

    Add instrument picker

--- 31DAYS/DAY_02/code/sketch.coffee
+++ 31DAYS/DAY_02/code/sketch.coffee
@@ -1,12 +1,111 @@
 configuration = 
   sequencer_intervals: [ 4, 8, 16, 32, 64 ]
-  interval_radius: 60
+  interval_radius: 65
   radius: 30
   size: 800
   tempo: 70
+  instruments: [
+    { color: 0xDDF57373 }
+    { color: 0xDDF5AE73 }
+    { color: 0xDD459393 }
+    { color: 0xDD5CC45C }
+  ]
+
+class Instrument 
+  constructor: (@color, @num) ->
+    @radius = 80
+    @x = 40
+    @y = (@num + 1) * 100
+    @selected = false
+
+  draw: (processing) ->
+    processing.stroke(if @selected then 0 else @color)
+    processing.fill(@color)
+    processing.ellipse @x, @y, @radius, @radius 
+
+  tap: (x, y) ->
+    if Math.sqrt(Math.pow(@x - x, 2) + Math.pow(@y - y, 2)) < @radius / 2
+      @selected = not(@selected)
+      true
+    else
+      false
+
+  unselect: () ->
+    @selected = false
+
+class InstrumentPicker
+  constructor: (config) ->
+    @selected_instrument = null
+    @instruments = []
+    for i in [0...config.length]
+      @instruments.push(new Instrument(config[i].color, i))
+
+  draw: (processing) ->
+    instrument.draw(processing) for instrument in @instruments
+  
+  tap: (x, y) ->
+    for instrument in @instruments
+      if instrument.tap(x, y)
+        @selected_instrument = instrument
+        for other_instrument in @instruments
+          other_instrument.unselect() if other_instrument.num != instrument.num
+        break
+
+
+class Note
+  constructor: (@note, @x, @y) ->
+    @radius = configuration.radius
+    @color = 0
+
+  draw: (processing, metronome, selected) ->
+    processing.noFill()
+    processing.stroke(0)
+    processing.fill(@color) if @note is 1
+    processing.fill(100) if selected
+    processing.ellipse @x, @y, @radius, @radius 
+
+  tap: (x, y, instrument) ->
+    if instrument and Math.sqrt(Math.pow(@x - x, 2) + Math.pow(@y - y, 2)) < @radius / 2
+      @note = if @note is 1 then 0 else 1
+      @color = instrument.color
+
+class Sequence
+  constructor: (@interval, @radius) ->
+    @notes = []
+    for i in [0...@interval]
+      angle = i * (2 * Math.PI / @interval)
+      x = 50 + (configuration.size / 2) + Math.cos(angle) * (@radius + 1) * configuration.interval_radius
+      y = (configuration.size / 2) + Math.sin(angle) * (@radius + 1) * configuration.interval_radius
+      @notes.push new Note(0, x, y)
+
+
+  draw: (processing, metronome) ->
+    for i in [0...@notes.length]
+      note = @notes[i]
+      selected = metronome.slice is i * (metronome.max_interval / @interval)
+      note.draw(processing, metronome, selected)
+
+
+  tap: (x, y, instrument) ->
+    note.tap(x, y, instrument) for note in @notes
+
+class Sequencer 
+  constructor: (@intervals) ->
+    @sequences = []
+    for radius in [0..@intervals.length]
+      interval = @intervals[radius]
+      @sequences.push new Sequence(interval, radius)
+
+  draw: (processing, metronome) ->
+    sequence.draw(processing, metronome) for sequence in @sequences
+
+  tap: (x, y, instrument) ->
+    sequence.tap(x, y, instrument) for sequence in @sequences
+
 
 class Metronome
   constructor: (@tempo) ->
+    @tempo = tempo
     @max_interval = Math.max.apply(null, configuration.sequencer_intervals)
     @running = false
   
@@ -29,27 +128,20 @@ sketch = (processing) ->
   
   processing.setup = () =>
     @metronome = new Metronome(configuration.tempo)
+    @sequencer = new Sequencer(configuration.sequencer_intervals)
+    @instrument_picker = new InstrumentPicker(configuration.instruments)
+
     @metronome.start()
 
-  draw_sequencer = (interval, j) =>
-    for i in [0..interval]
-      angle = i * (2 * Math.PI / interval)
-      x = (configuration.size / 2) + Math.cos(angle) * (j + 1) * configuration.interval_radius
-      y = (configuration.size / 2) + Math.sin(angle) * (j + 1) * configuration.interval_radius
-      processing.noFill()
-      processing.stroke(0)
-      if @metronome.slice is i * (@metronome.max_interval / interval)
-        processing.fill(0)
-      processing.ellipse x, y, configuration.radius, configuration.radius
-
-
-  draw_sequencers = () ->
-    draw_sequencer(configuration.sequencer_intervals[i], i) for i in [0..configuration.sequencer_intervals.length]
-      
-
-  processing.draw = () ->
+  processing.draw = () =>
     processing.background(255)
-    draw_sequencers()
+    @sequencer.draw(processing, @metronome)
+    @instrument_picker.draw(processing)
+
+  processing.mousePressed = () =>
+    [ x, y ] = [ processing.mouseX, processing.mouseY ]
+    @instrument_picker.tap(x, y)
+    @sequencer.tap(x, y, @instrument_picker.selected_instrument)
 
 
 canvas = document.getElementById "canvas"
--- 31DAYS/DAY_02/code/sketch.js
+++ 31DAYS/DAY_02/code/sketch.js
@@ -1,16 +1,218 @@
 (function() {
-  var Metronome, canvas, configuration, p, sketch;
+  var Instrument, InstrumentPicker, Metronome, Note, Sequence, Sequencer, canvas, configuration, p, sketch;
 
   configuration = {
     sequencer_intervals: [4, 8, 16, 32, 64],
-    interval_radius: 60,
+    interval_radius: 65,
     radius: 30,
     size: 800,
-    tempo: 70
+    tempo: 70,
+    instruments: [
+      {
+        color: 0xDDF57373
+      }, {
+        color: 0xDDF5AE73
+      }, {
+        color: 0xDD459393
+      }, {
+        color: 0xDD5CC45C
+      }
+    ]
   };
+
+  Instrument = (function() {
+    function Instrument(color, num) {
+      this.color = color;
+      this.num = num;
+      this.radius = 80;
+      this.x = 40;
+      this.y = (this.num + 1) * 100;
+      this.selected = false;
+    }
+
+    Instrument.prototype.draw = function(processing) {
+      processing.stroke(this.selected ? 0 : this.color);
+      processing.fill(this.color);
+      return processing.ellipse(this.x, this.y, this.radius, this.radius);
+    };
+
+    Instrument.prototype.tap = function(x, y) {
+      if (Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)) < this.radius / 2) {
+        this.selected = !this.selected;
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    Instrument.prototype.unselect = function() {
+      return this.selected = false;
+    };
+
+    return Instrument;
+
+  })();
+
+  InstrumentPicker = (function() {
+    function InstrumentPicker(config) {
+      var i, _i, _ref;
+      this.selected_instrument = null;
+      this.instruments = [];
+      for (i = _i = 0, _ref = config.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        this.instruments.push(new Instrument(config[i].color, i));
+      }
+    }
+
+    InstrumentPicker.prototype.draw = function(processing) {
+      var instrument, _i, _len, _ref, _results;
+      _ref = this.instruments;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        instrument = _ref[_i];
+        _results.push(instrument.draw(processing));
+      }
+      return _results;
+    };
+
+    InstrumentPicker.prototype.tap = function(x, y) {
+      var instrument, other_instrument, _i, _j, _len, _len1, _ref, _ref1, _results;
+      _ref = this.instruments;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        instrument = _ref[_i];
+        if (instrument.tap(x, y)) {
+          this.selected_instrument = instrument;
+          _ref1 = this.instruments;
+          for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+            other_instrument = _ref1[_j];
+            if (other_instrument.num !== instrument.num) {
+              other_instrument.unselect();
+            }
+          }
+          break;
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    return InstrumentPicker;
+
+  })();
+
+  Note = (function() {
+    function Note(note, x, y) {
+      this.note = note;
+      this.x = x;
+      this.y = y;
+      this.radius = configuration.radius;
+      this.color = 0;
+    }
+
+    Note.prototype.draw = function(processing, metronome, selected) {
+      processing.noFill();
+      processing.stroke(0);
+      if (this.note === 1) {
+        processing.fill(this.color);
+      }
+      if (selected) {
+        processing.fill(100);
+      }
+      return processing.ellipse(this.x, this.y, this.radius, this.radius);
+    };
+
+    Note.prototype.tap = function(x, y, instrument) {
+      if (instrument && Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)) < this.radius / 2) {
+        this.note = this.note === 1 ? 0 : 1;
+        return this.color = instrument.color;
+      }
+    };
+
+    return Note;
+
+  })();
+
+  Sequence = (function() {
+    function Sequence(interval, radius) {
+      var angle, i, x, y, _i, _ref;
+      this.interval = interval;
+      this.radius = radius;
+      this.notes = [];
+      for (i = _i = 0, _ref = this.interval; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        angle = i * (2 * Math.PI / this.interval);
+        x = 50 + (configuration.size / 2) + Math.cos(angle) * (this.radius + 1) * configuration.interval_radius;
+        y = (configuration.size / 2) + Math.sin(angle) * (this.radius + 1) * configuration.interval_radius;
+        this.notes.push(new Note(0, x, y));
+      }
+    }
+
+    Sequence.prototype.draw = function(processing, metronome) {
+      var i, note, selected, _i, _ref, _results;
+      _results = [];
+      for (i = _i = 0, _ref = this.notes.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        note = this.notes[i];
+        selected = metronome.slice === i * (metronome.max_interval / this.interval);
+        _results.push(note.draw(processing, metronome, selected));
+      }
+      return _results;
+    };
+
+    Sequence.prototype.tap = function(x, y, instrument) {
+      var note, _i, _len, _ref, _results;
+      _ref = this.notes;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        note = _ref[_i];
+        _results.push(note.tap(x, y, instrument));
+      }
+      return _results;
+    };
+
+    return Sequence;
+
+  })();
+
+  Sequencer = (function() {
+    function Sequencer(intervals) {
+      var interval, radius, _i, _ref;
+      this.intervals = intervals;
+      this.sequences = [];
+      for (radius = _i = 0, _ref = this.intervals.length; 0 <= _ref ? _i <= _ref : _i >= _ref; radius = 0 <= _ref ? ++_i : --_i) {
+        interval = this.intervals[radius];
+        this.sequences.push(new Sequence(interval, radius));
+      }
+    }
+
+    Sequencer.prototype.draw = function(processing, metronome) {
+      var sequence, _i, _len, _ref, _results;
+      _ref = this.sequences;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        sequence = _ref[_i];
+        _results.push(sequence.draw(processing, metronome));
+      }
+      return _results;
+    };
+
+    Sequencer.prototype.tap = function(x, y, instrument) {
+      var sequence, _i, _len, _ref, _results;
+      _ref = this.sequences;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        sequence = _ref[_i];
+        _results.push(sequence.tap(x, y, instrument));
+      }
+      return _results;
+    };
+
+    return Sequencer;
+
+  })();
 
   Metronome = (function() {
     function Metronome(tempo) {
+      this.tempo = tempo;
       this.tempo = tempo;
       this.max_interval = Math.max.apply(null, configuration.sequencer_intervals);
       this.running = false;
@@ -43,43 +245,29 @@
   })();
 
   sketch = function(processing) {
-    var draw_sequencer, draw_sequencers;
     processing.setup = (function(_this) {
       return function() {
         _this.metronome = new Metronome(configuration.tempo);
+        _this.sequencer = new Sequencer(configuration.sequencer_intervals);
+        _this.instrument_picker = new InstrumentPicker(configuration.instruments);
         return _this.metronome.start();
       };
     })(this);
-    draw_sequencer = (function(_this) {
-      return function(interval, j) {
-        var angle, i, x, y, _i, _results;
-        _results = [];
-        for (i = _i = 0; 0 <= interval ? _i <= interval : _i >= interval; i = 0 <= interval ? ++_i : --_i) {
-          angle = i * (2 * Math.PI / interval);
-          x = (configuration.size / 2) + Math.cos(angle) * (j + 1) * configuration.interval_radius;
-          y = (configuration.size / 2) + Math.sin(angle) * (j + 1) * configuration.interval_radius;
-          processing.noFill();
-          processing.stroke(0);
-          if (_this.metronome.slice === i * (_this.metronome.max_interval / interval)) {
-            processing.fill(0);
-          }
-          _results.push(processing.ellipse(x, y, configuration.radius, configuration.radius));
-        }
-        return _results;
+    processing.draw = (function(_this) {
+      return function() {
+        processing.background(255);
+        _this.sequencer.draw(processing, _this.metronome);
+        return _this.instrument_picker.draw(processing);
       };
     })(this);
-    draw_sequencers = function() {
-      var i, _i, _ref, _results;
-      _results = [];
-      for (i = _i = 0, _ref = configuration.sequencer_intervals.length; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
-        _results.push(draw_sequencer(configuration.sequencer_intervals[i], i));
-      }
-      return _results;
-    };
-    return processing.draw = function() {
-      processing.background(255);
-      return draw_sequencers();
-    };
+    return processing.mousePressed = (function(_this) {
+      return function() {
+        var x, y, _ref;
+        _ref = [processing.mouseX, processing.mouseY], x = _ref[0], y = _ref[1];
+        _this.instrument_picker.tap(x, y);
+        return _this.sequencer.tap(x, y, _this.instrument_picker.selected_instrument);
+      };
+    })(this);
   };
 
   canvas = document.getElementById("canvas");
